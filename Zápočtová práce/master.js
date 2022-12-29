ab66ecc6f6552e0eca06e4b3f6473f83324de3db
Kevin Daněk <heindrich73@gmail.com>
2022-12-29 21:02:31 +0100

    Ta blbost funguje.

--- Zápočtová práce/master.js
+++ Zápočtová práce/master.js
@@ -1,76 +1,123 @@
-//https://cainos.itch.io/pixel-art-top-down-basic
+// Colours skillfully yoinked from TailwindCSS
+const ANCHOR_AMOUNT = 16;
+const ANCHORS = [];
+const COLOURS = ["#ef4444", "#f97316", "#f59e0b", "#eab308", "#84cc16", "#22c55e", "#10b981", "#14b8a6", "#06b6d4", "#0ea5e9", "#3b82f6", "#6366f1", "#8b5cf6", "#a855f7", "#d946ef", "#ec4899", "#f43f5e"];
+const SIZE = 64;
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+const TILES_PER_WIDTH = Math.ceil(WIDTH / SIZE);
+const TILES_PER_HEIGHT = Math.ceil(HEIGHT / SIZE);
+const TILES = [...Array(TILES_PER_HEIGHT)].map(_ => [...Array(TILES_PER_WIDTH)]);
 
-/*
-    Step 1: Nějak narandom vygenerovat povrch
-    Step 2: Potom řešit spoje
+const fragment = document.createDocumentFragment();
 
-    Mít mapu možných kombinací a nějak iterovat / náhodně vybírat??? (+ asi nějakej stack pro historii, kdyby se něco dojebalo)
-*/
+const getRandomNumberFromInterval = (a, b) => Math.floor(Math.random() * (b - a + 1) + a);
+const getRandomColour = () => getRandomNumberFromInterval(0, COLOURS.length - 1);
+const getAnchorDistances = (x, y, arr = ANCHORS.slice()) => arr.slice().map(v => JSON.parse(v)).map((v, _) => Math.round(Math.sqrt(Math.pow(v[0] - y, 2) + Math.pow(v[1] - x, 2))));
 
-const TIMERS = {
-    grid: "Grid Render"
+function cyclic(n, length) {
+    // ToInteger() abstract op
+    n = Math.trunc(n) || 0;
+    // Allow negative indexing from the end
+    if (n < 0) n += length;
+    // OOB access is guaranteed to return undefined
+    if (n >= length) n %= length;
+    // Otherwise, this is just normal property access
+    return n;
 }
 
-const TESTRULES = {
-    "red": ["pink", "red", "orange"],
-    "orange": ["red", "orange", "yellow"],
-    "yellow": ["orange", "yellow", "yellowgreen"],
-    "yellowgreen": ["yellow", "yellowgreen", "green"],
-    "green": ["yellowgreen", "green", "teal"],
-    "teal": ["green", "teal", "blue"],
-    "blue": ["teal", "blue", "darkslateblue"],
-    "darkslateblue": ["blue", "darkslateblue", "purple"],
-    "purple": ["darkslateblue", "purple", "fuchsia"],
-    "fuchsia": ["purple", "fuchsia", "pink"],
-    "pink": ["fuchsia", "pink", "red"],
-}
 
-const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)]
+/**
+ * Funkce pro vykreslení mřížky s dlaždicemi
+ */
+function RenderGrid() {
+    let rowIndex = 0;
+    let colIndex = 0;
 
-function grid(target) {
-    const SIZE = 32;
-    const WIDTH = window.innerWidth;
-    const HEIGHT = window.innerHeight;
-    const TILES_PER_WIDTH = Math.ceil(WIDTH / SIZE);
-    const TILES_PER_HEIGHT = Math.ceil(HEIGHT / SIZE);
-    const fragment = new DocumentFragment();
-    
-    console.log(TILES_PER_WIDTH, TILES_PER_HEIGHT);
-    
-    console.time(TIMERS.grid);
-    for (let index = 0; index < TILES_PER_WIDTH * TILES_PER_HEIGHT; index++) {
-        const tile = document.createElement("div");
-        let colour;
+    for (const ROW of TILES) {
+        colIndex = 0;
 
-        if (index == 0) {
-            colour = getRandom(Object.keys(TESTRULES));
-        }
-        else {
-            colour = "white";
+        for (const TILE of ROW) {
+            const tileContainer = document.createElement("div");
+            const colour = COLOURS.at(cyclic(TILE, COLOURS.length));
+
+            tileContainer.style = `background-color: ${colour}; width: ${SIZE}px; height: ${SIZE}px;`;
+            tileContainer.id = `${rowIndex}_${colIndex}`;
+            tileContainer.setAttribute("data-tile", TILE);
+
+            /*if (ANCHORS.includes(`[${rowIndex},${colIndex}]`)) {
+                tileContainer.style = `background-color: ${colour}; width: ${SIZE}px; height: ${SIZE}px; box-shadow: inset 0 0 10px #000;`
+            }*/
+
+            fragment.append(tileContainer);
+            colIndex++;
         }
 
-        
-        //const colour = Math.floor(Math.random()*16777215).toString(16);
-
-
-        tile.style = `background: ${colour}; width: ${SIZE}px; height: ${SIZE}px`;
-        tile.id = `${Math.floor(index / TILES_PER_WIDTH)}_${index % TILES_PER_WIDTH}`;
-
-        fragment.append(tile);
-
-
-
-
+        rowIndex++;
     }
 
-    console.timeLog(TIMERS.grid);
-
-    requestAnimationFrame(() => {
-        target.append(fragment);
-        console.timeEnd(TIMERS.grid);
-    });
+    document.body.append(fragment);
+    //requestAnimationFrame(() => {
+    //});
 }
 
+/**
+ * Funkce pro vytvoření gridu na obrazovce
+ */
+function main() {
+    //Vytvoření kotev
+    for (let index = 0; index < ANCHOR_AMOUNT; index++) {
+        const RandomX = getRandomNumberFromInterval(0, TILES_PER_WIDTH - 1);
+        const RandomY = getRandomNumberFromInterval(0, TILES_PER_HEIGHT - 1);
+        const RandomColour = getRandomColour();
 
+        ANCHORS.push(`[${RandomY},${RandomX}]`);
+        TILES[RandomY][RandomX] = RandomColour;
+    }
 
-grid(document.body);
+    //Vytvoření zón
+    for (let index = 0; index < TILES_PER_WIDTH * TILES_PER_HEIGHT; index++) {
+        const X = index % TILES_PER_WIDTH;
+        const Y = Math.floor(index / TILES_PER_WIDTH);
+
+        if (ANCHORS.includes(`[${Y},${X}]`)) continue;
+
+        const distances = getAnchorDistances(X, Y);
+        const minimum = Math.min(...distances);
+        const minimumIndex = distances.indexOf(minimum);
+        const [colourY, colourX] = JSON.parse(ANCHORS[minimumIndex]);
+
+        TILES[Y][X] = TILES[colourY][colourX];
+    }
+
+    //Blending po ose X
+    for (let index = 0; index < TILES_PER_WIDTH * TILES_PER_HEIGHT; index++) {
+        const X = index % TILES_PER_WIDTH;
+        const Y = Math.floor(index / TILES_PER_WIDTH);
+
+        const dx = TILES[Y][X] - TILES[Y][Math.max(X - 1, 0)]
+
+        if (Math.abs(dx) > 1) {
+            const colour = TILES[Y][Math.max(X - 1, 0)] + Math.sign(dx);
+            TILES[Y][X] = colour;
+        }
+    }
+
+    //Blending po ose Y
+    for (let index = 0; index < TILES_PER_WIDTH * TILES_PER_HEIGHT; index++) {
+        const Y = index % TILES_PER_HEIGHT;
+        const X = Math.floor(index / TILES_PER_HEIGHT);
+
+        const dy = TILES[Y][X] - TILES[Math.max(Y - 1, 0)][X]
+
+        if (Math.abs(dy) > 1) {
+            const colour = TILES[Math.max(Y - 1, 0)][X] + Math.sign(dy);
+            TILES[Y][X] = colour;
+        }
+    }
+
+    RenderGrid();
+}
+
+main();
+delete TILES;
