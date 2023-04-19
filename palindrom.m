60a2090560daef3e690c9967ff39bcfabf8fda59
Kevin Daněk <heindrich73@gmail.com>
2023-04-19 13:29:17 +0200

    03 Palindrom

--- palindrom.m
+++ palindrom.m
@@ -1,2 +1,13 @@
-clc; clear; close all;
-
+function [str_result] = palindrom(x)
+    x = lower(x);
+    len = length(x);
+    leftBound = floor(len / 2);
+    rightBound = ceil(len / 2) + 1;
+    result = all(x(1:leftBound) == flip(x(rightBound:len)));
+    
+    if result
+        str_result = 'ano';
+    else
+        str_result = 'ne';
+    end
+end
---
+++ palindromTest.m
@@ -0,0 +1,8 @@
+%% Test 1: Jelen
+assert(strcmp(palindrom('Jelen'),'ne'))
+
+%% Test 2: Kajak
+assert(strcmp(palindrom('Kajak'),'ano'))
+
+%% Test 3: anna
+assert(strcmp(palindrom('anna'),'ano'))
